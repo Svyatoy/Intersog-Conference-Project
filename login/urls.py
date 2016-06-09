9ff5f75d45ae2bef143a8af48693732c6478e23b
Svyatoy <kreyser2007@mail.ru>
2016-06-09 16:09:57 +0300

    Update urls.py

--- login/urls.py
+++ login/urls.py
@@ -6,6 +6,6 @@ urlpatterns = [
     url(r'^$', UserView.as_view(), name='user_profile'),
     url(r'^/edit$', UserView.as_view(), name='user_profile'),
     url(r'^users/(?P<pk>\d+)/$', UserAccountView.as_view(), name='user_account'),
-    # url(r'^edit$', EditUserProfile.as_view(), name='edit_user_profile'),
+
     url(r'^messages/', include('messanges.urls')),
 ]
